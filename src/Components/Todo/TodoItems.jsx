de401017a5acd40ad72c385f08e05c87834293a0
nehalpanchall <nehalpanchal40@gmail.com>
2025-01-15 09:47:17 -0500

    add one more Todo Item and evaluated name/date using Array

--- src/Components/Todo/TodoItems.jsx
+++ src/Components/Todo/TodoItems.jsx
@@ -1,13 +1,21 @@
 const TodoItems = () => {
-  let todoName = "Buy milk";
-  let todoDate = "11/06/2025";
+  let todoName = ["Buy milk", "Practice JavaScript"];
+  let todoDate = ["11/06/2025", "18/06/2024"];
 
   return (
     <>
       <div class="bg-white p-4 rounded-lg shadow-md">
         <div class="flex justify-between items-center py-2 border-b border-gray-200">
-          <span class="text-gray-700">{todoName}</span>
-          <span class="text-gray-500 text-sm">{todoDate}</span>
+          <span class="text-gray-700">{todoName[0]}</span>
+          <span class="text-gray-500 text-sm">{todoDate[0]}</span>
+          <button class="text-red-500 hover:text-red-600">Delete</button>
+        </div>
+      </div>
+
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <div class="flex justify-between items-center py-2 border-b border-gray-200">
+          <span class="text-gray-700">{todoName[1]}</span>
+          <span class="text-gray-500 text-sm">{todoDate[1]}</span>
           <button class="text-red-500 hover:text-red-600">Delete</button>
         </div>
       </div>
