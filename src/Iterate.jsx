08c394d668a62b40800f9f43815bd887a104061e
nehalpanchall <nehalpanchal40@gmail.com>
2025-01-18 09:52:54 -0500

    add CSS modules in Reactify for individual component

--- src/Iterate.jsx
+++ src/Iterate.jsx
@@ -1,5 +1,7 @@
 import IterateProps from './Components/IterateProps';
 
+import css from './Iterate.module.css';
+
 const Iterate = () => {
   const games = ['God of War', 'Spider Man', 'Call of Duty', 'GTA 6'];
 
@@ -7,14 +9,16 @@ const Iterate = () => {
 
   return (
     <>
-      <h1 className="ml-5 font-bold">List of PS5 Games</h1>
+      <h1 className={`${css['back-ground']} ${css.games} font-bold`}>
+        List of PS5 Games
+      </h1>
 
       <IterateProps games={games} x={6} />
 
       <br />
 
       {/* Explitcit Return: With curly braces */}
-      <h1 className="ml-5 font-bold">List of Web Series</h1>
+      <h1 className={css['web-series']}>List of Web Series</h1>
       <ul className="list-disc ml-7">
         {webSeries.map((items, index) => {
           return <li key={index}>{items}</li>;
