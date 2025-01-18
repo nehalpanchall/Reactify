9e7982c2b0c4837206aabb9bfb619a18c4d39e56
nehalpanchall <nehalpanchal40@gmail.com>
2025-01-18 12:37:46 -0500

    pass array of buttons in props from parent component

--- src/Components/Calc/ButtonsContainer.jsx
+++ src/Components/Calc/ButtonsContainer.jsx
@@ -1,12 +1,10 @@
 import css from './Calculator.module.css';
 
-const ButtonsContainer = () => {
-  let operations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '+', '-', '*', '/', 'C'];
-
+const ButtonsContainer = ({ keypad }) => {
   return (
     <>
       <div className={css.displayButtons}>
-        {operations.map((items, index) => {
+        {keypad.map((items, index) => {
           return <button key={index}>{items}</button>;
         })}
       </div>
--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -3,13 +3,15 @@ import css from './Calc/Calculator.module.css';
 import ButtonsContainer from './Calc/ButtonsContainer';
 
 const Calculator = () => {
+  let buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '+', '-', '*', '/', 'C'];
+
   return (
     <>
       <div className={css.body}>
         <div className={css.mainContainer}>
           <Display />
 
-          <ButtonsContainer />
+          <ButtonsContainer keypad={buttons} />
         </div>
       </div>
     </>
