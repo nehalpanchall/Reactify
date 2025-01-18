b76eddabdfca10343dbcd3d9f4156c3759d0bbcc
nehalpanchall <nehalpanchal40@gmail.com>
2025-01-18 10:20:41 -0500

    convert arrays of todoName and todoDate into single array of object and pass as props

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -5,15 +5,18 @@ import TodoTitle from './Todo/TodoTitle';
 const Todo = () => {
   let title = 'Todo with props';
 
-  let todoName = ['Buy milk', 'Practice JavaScript'];
-  let todoDate = ['11/06/2025', '18/06/2024'];
+  let todoItems = [
+    { todoName: 'Buy Milk', todoDate: '11/06/2025' },
+    { todoName: 'Practice JavaScript', todoDate: '18/06/2024' },
+  ];
+
   return (
     <>
       <div className="bg-gray-100 p-6 rounded-lg shadow-md max-w-lg mx-auto mt-10">
         <TodoTitle newTitle={title} />
         <TodoAdd />
 
-        <TodoItems names={todoName} dates={todoDate} />
+        <TodoItems todoItems={todoItems} />
       </div>
     </>
   );
--- src/Components/Todo/TodoItems.jsx
+++ src/Components/Todo/TodoItems.jsx
@@ -1,18 +1,26 @@
-const TodoItems = ({ names, dates }) => {
+const TodoItems = ({ todoItems }) => {
+  console.log();
+
+  // for (let i of todoItems) {
+  //   for (let j in i) {
+  //     console.log(i[j]);
+  //   }
+  // }
+
   return (
     <>
       <div className="bg-white p-4 rounded-lg shadow-md">
         <div className="flex justify-between items-center py-2 border-b border-gray-200">
-          <span className="text-gray-700">{names[0]}</span>
-          <span className="text-gray-500 text-sm">{dates[0]}</span>
+          <span className="text-gray-700">{todoItems[0].todoName}</span>
+          <span className="text-gray-500 text-sm">{todoItems[0].todoDate}</span>
           <button className="text-red-500 hover:text-red-600">Delete</button>
         </div>
       </div>
 
       <div className="bg-white p-4 rounded-lg shadow-md">
         <div className="flex justify-between items-center py-2 border-b border-gray-200">
-          <span className="text-gray-700">{names[1]}</span>
-          <span className="text-gray-500 text-sm">{dates[1]}</span>
+          <span className="text-gray-700">{todoItems[1].todoName}</span>
+          <span className="text-gray-500 text-sm">{todoItems[1].todoDate}</span>
           <button className="text-red-500 hover:text-red-600">Delete</button>
         </div>
       </div>
