9cb99cf214bd6db1210136d9090359bbdea93235
nehalpanchall <nehalpanchal40@gmail.com>
2025-01-20 11:29:16 -0500

    fix some minor bugs

--- src/Components/Food Menu/FoodContainer.jsx
+++ src/Components/Food Menu/FoodContainer.jsx
@@ -1,7 +1,6 @@
 import css from './FoodContainer.module.css';
 
 const FoodContainer = props => {
-  console.log(props);
   return <div className={css.container}>{props.children}</div>;
 };
 
--- src/Components/Food Menu/FoodItems.jsx
+++ src/Components/Food Menu/FoodItems.jsx
@@ -1,13 +1,18 @@
 import css from './FoodMenu.module.css';
 
 const FoodItems = ({ FoodItems }) => {
-  console.log(FoodItems);
+  const handleClick = event => {
+    console.log(event.target);
+  };
   return (
     <>
       {FoodItems.map((items, index) => {
         return (
           <ul key={index}>
             <li className={css.items}>{items}</li>
+            <button onClick={handleClick} className={css.foodButton}>
+              Buy Me
+            </button>
           </ul>
         );
       })}
