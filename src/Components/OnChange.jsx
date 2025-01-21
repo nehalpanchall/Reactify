52bb510ad2168fff89ebd19689b8b680cd9645b8
nehalpanchall <nehalpanchal40@gmail.com>
2025-01-21 10:50:46 -0500

    add items onClick using useState() into array and spread operator

--- src/Components/OnChange.jsx
+++ src/Components/OnChange.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react';
 
 const onChange = () => {
-  let stateArr = useState('Text here...');
+  let [items, setItems] = useState([]);
+
+  function addItem() {
+    let newItems = [...items, displayText];
+    setItems(newItems);
+    handleClear();
+  }
+
+  let stateArr = useState('');
   let displayText = stateArr[0];
   let setDisplayText = stateArr[1];
 
   const handleChange = event => {
-    // console.log(event.target.value);
-
     setDisplayText(event.target.value);
+    return event.target.value;
   };
 
-  const handleClick = () => {
-    let textField = document.querySelector('#textField');
-    textField.value = '';
+  const handleClear = () => {
+    document.querySelector('#textField').value = '';
     setDisplayText('');
+    document.querySelector('#textField').focus();
   };
 
   return (
@@ -25,15 +32,15 @@ const onChange = () => {
         id="textField"
         placeholder="Enter text.."
         onChange={handleChange}
+        autoFocus
       />
 
       <br />
-      <div className="ml-8">{displayText}</div>
+      <div className="ml-8">Entered Input is: {displayText}</div>
 
       <button
-        onClick={handleClick}
+        onClick={handleClear}
         style={{
-          backgroundColor: 'gray',
           border: '1px solid black',
           padding: '10px 50px',
           margin: '30px'
@@ -41,6 +48,23 @@ const onChange = () => {
       >
         Clear
       </button>
+
+      <button
+        onClick={addItem}
+        style={{
+          border: '1px solid black',
+          padding: '10px 50px',
+          margin: '30px'
+        }}
+      >
+        Add
+      </button>
+
+      <ul className="ml-10">
+        {items.map((i, index) => (
+          <li key={index}>{i}</li>
+        ))}
+      </ul>
     </>
   );
 };
