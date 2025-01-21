5368fc8cb4ccd22dacf526ae5716fc7a13370b79
nehalpanchall <nehalpanchal40@gmail.com>
2025-01-21 09:41:17 -0500

    handle onChange using useState()

--- src/Components/OnChange.jsx
+++ src/Components/OnChange.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
+
 const onChange = () => {
+  let stateArr = useState('Text here...');
+  let displayText = stateArr[0];
+  let setDisplayText = stateArr[1];
+
   const handleChange = event => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
+
+    setDisplayText(event.target.value);
   };
 
   const handleClick = () => {
     let textField = document.querySelector('#textField');
     textField.value = '';
+    setDisplayText('');
   };
 
   return (
@@ -17,6 +26,9 @@ const onChange = () => {
         placeholder="Enter text.."
         onChange={handleChange}
       />
+
+      <br />
+      <div className="ml-8">{displayText}</div>
 
       <button
         onClick={handleClick}
