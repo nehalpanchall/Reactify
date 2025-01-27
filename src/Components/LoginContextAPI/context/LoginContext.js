e3a909bce5cacff49e9babaf09cb6b702895800c
nehalpanchall <nehalpanchal40@gmail.com>
2025-01-27 10:33:56 -0500

    initialize createContext with empty object

--- src/Components/LoginContextAPI/context/LoginContext.js
+++ src/Components/LoginContextAPI/context/LoginContext.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
 
-const LoginContextObj = createContext();
+const LoginContextObj = createContext({ user: '', setUser: () => {} });
 
 export default LoginContextObj;
