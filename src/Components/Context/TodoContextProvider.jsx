02d2296603fd9607c51ee70c3c4ab5afd745a558
nehalpanchall <nehalpanchal40@gmail.com>
2025-01-27 08:26:33 -0500

    create separate Provider file and connect as children props with parent component

---
+++ src/Components/Context/TodoContextProvider.jsx
@@ -0,0 +1,37 @@
+// TodoProvider.jsx
+import React, { useState } from 'react';
+import TodoContext from './TodoContext';
+
+const TodoProvider = ({ children }) => {
+  let title = 'Todo with Context';
+
+  const [todoItems, setTodoItems] = useState([
+    { todoName: 'Buy Milk', todoDate: '11/06/2025' },
+  ]);
+
+  const getTodoItem = (name, date) => {
+    let newItem = { todoName: name, todoDate: date };
+    setTodoItems((currentArr) => [...currentArr, newItem]);
+  };
+
+  const deleteItem = (deleteItem) => {
+    setTodoItems((currentArr) =>
+      currentArr.filter((item) => item !== deleteItem)
+    );
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todoItems,
+        deleteItem,
+        getTodoItem,
+        title,
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoProvider;
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,49 +1,18 @@
-import { useState } from 'react';
+import TodoContextProvider from './Context/TodoContextProvider';
 import TodoAdd from './Todo/TodoAdd';
 import TodoItems from './Todo/TodoItems';
 import TodoTitle from './Todo/TodoTitle';
-import TodoContext from './Context/TodoContext';
 
 const Todo = () => {
-  let title = 'Todo with props';
-
-  const [todoItems, setTodoItems] = useState([
-    { todoName: 'Buy Milk', todoDate: '11/06/2025' },
-  ]);
-
-  const getTodoItem = (name, date) => {
-    let newItem = { todoName: name, todoDate: date };
-    setTodoItems((currentArr) => {
-      let newArr = [...currentArr, newItem];
-      return newArr;
-    });
-
-    // name.value = '';
-    // date.value = '';
-    // name.focus();
-  };
-
-  const deleteItem = (deleteItem) => {
-    let notDelete = todoItems.filter((item) => item !== deleteItem);
-    setTodoItems(notDelete);
-  };
-
   return (
-    <TodoContext.Provider
-      value={{
-        todoItems,
-        deleteItem,
-        getTodoItem,
-        title,
-      }}
-    >
+    <TodoContextProvider>
       <div className="bg-gray-100 p-6 rounded-lg shadow-md max-w-lg mx-auto mt-10">
         <TodoTitle />
         <TodoAdd />
 
         <TodoItems />
       </div>
-    </TodoContext.Provider>
+    </TodoContextProvider>
   );
 };
 
